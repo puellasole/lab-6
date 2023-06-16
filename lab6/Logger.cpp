20de38898512e8d1776352363e53c56cd0943552
Daria Arias <117343467+puellasole@users.noreply.github.com>
2023-06-16 15:32:44 +0200

    Update Logger.cpp

--- lab6/Logger.cpp
+++ lab6/Logger.cpp
@@ -1,8 +1,4 @@
 #include "Logger.hpp"
 
-namespace logger {
-    std::ofstream Logger::outStream;
-    Level Logger::level = Level::DEBUG;
-
-
-}
+std::ofstream esp::Log::m_out;
+esp::LogLvl esp::Log::m_loglvl;
